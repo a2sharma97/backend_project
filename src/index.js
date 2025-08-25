15a2b5bf25ea2adf7ea3c67e161e0878a9e5bac5
Abhishek Sharma <a2sharma97@gmail.com>
2025-08-25 14:15:53 +0530

    add error api, response api, async handler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
+connectDB() //this is an async mehthod so it  returns promise so we can handle promises by using .then();
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("App.ication is unable to connect with the database", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port :  ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed !!!", error);
+  });
 //1st approach to connect db
 /*import express from "express";
 const app = express();
